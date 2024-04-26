d5ac40796ffb8c1e25b1d1784ca45ff83fb71ea7
sneha <sneha.varshney_cs19@gla.ac.in>
2024-04-26 23:34:21 +0530

    Added form componenrt

---
+++ form/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './Components/form';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Form</h1>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
