1f4d3ed8514c343a0230dc727d8446d6d03683a0
sneha <sneha.varshney_cs19@gla.ac.in>
2024-07-21 20:21:51 +0530

    Added grid layout

--- BookLibrary/src/Components/Books.jsx
+++ BookLibrary/src/Components/Books.jsx
@@ -17,27 +17,28 @@ function Books() {
         onClose={() => setIsModalOpen(false)}
         onBookAdded={handleAddBook}
       />
-      <div className="mt-4 grid grid-cols-3 gap-6">
+      <div className="mt-4 w-full">
         {books.length === 0 ? (
-          <p>No books available</p>
+          <p className="text-center">No books available</p>
         ) : (
-          books.map((book) => (
-            <div
-              key={book._id}
-              className="border border-gray-300 rounded-md shadow-md p-6"
-            >
-              <h3 className="text-xl font-semibold">{book.title}</h3>
-              <img
-                src={`http://localhost:5000${book.image}`}
-                alt={book.title}
-                className="mt-2 w-full h-48 object-cover p-2 border border-blue-400"
-              />
-              <p className="mt-2">{book.description}</p>
-            </div>
-          ))
+          <div className="grid grid-cols-3 gap-6">
+            {books.map((book) => (
+              <div
+                key={book._id}
+                className="border border-gray-300 rounded-md shadow-md p-6"
+              >
+                <h3 className="text-xl font-semibold">{book.title}</h3>
+                <img
+                  src={`http://localhost:5000${book.image}`}
+                  alt={book.title}
+                  className="mt-2 w-full h-48 object-cover p-2 border border-blue-400"
+                />
+                <p className="mt-2">{book.description}</p>
+              </div>
+            ))}
+          </div>
         )}
       </div>
-
       <button
         className="bg-blue-400 text-white py-2 px-4 rounded mt-5"
         onClick={() => setIsModalOpen(true)}
